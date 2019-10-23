94bd9ada033c6298ec9901e765f0b30379ed5f7f
Vohlaan <patrick.burkhardt@gmx.ch>
2019-10-23 09:41:24 +0200

    Add Models, Create Folders

---
+++ src/models/Album.ts
@@ -0,0 +1,4 @@
+export interface Album{
+    Id: number;
+    Name: string;
+}
---
+++ src/models/Song.ts
@@ -0,0 +1,6 @@
+export interface Song{
+    Id: number;
+    Name: string;
+    Interpret: string;
+    AlbumId?: number;
+}
